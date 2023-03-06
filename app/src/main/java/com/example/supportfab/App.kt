87887430d215921c1cf65fd1b2cf8800ae1c9b25
Arvin Kim Marquez <arvinkim.marquez@finastra.com>
2023-03-06 22:39:46 +0800

    Removed unnecessary imports

--- app/src/main/java/com/example/supportfab/App.kt
+++ app/src/main/java/com/example/supportfab/App.kt
@@ -1,12 +1,7 @@
 package com.example.supportfab
 
 import android.app.Application
-import android.util.Log
-import com.example.supportfab.custom_fab.FabPosition
 import com.example.supportfab.custom_fab.GlobalFabStateManager
-import com.example.supportfab.custom_fab.SupportFabSharedPropertyController
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 private const val TAG = "App"
 
